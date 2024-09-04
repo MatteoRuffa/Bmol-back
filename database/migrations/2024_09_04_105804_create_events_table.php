40e60787782f0f1670f7d0be4bb1e710da642188
MatteoRuffa <cortese_17@protonmail.com>
2024-09-04 13:11:05 +0200

    add event_migration

--- database/migrations/2024_09_04_105804_create_events_table.php
+++ database/migrations/2024_09_04_105804_create_events_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug', 255)->unique();
+            $table->string('image_cover', 255)->nullable();
+            $table->date('date');
+            $table->time('hour');
+            $table->text('description');
+            $table->boolean('visibility');
             $table->timestamps();
         });
     }
