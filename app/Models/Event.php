f358fa365dd567558de9c4b7eed886784f053d3b
MatteoRuffa <cortese_17@protonmail.com>
2024-09-04 13:08:22 +0200

    add event_model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,8 +4,45 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Event extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name', 
+        'slug', 
+        'image_cover',
+        'date',
+        'hour',
+        'description',
+        'visibility',
+    ];
+
+    public static function generateSlug($name){
+        $slugBase = Str::slug(trim($name), '-');
+        $slugs = \App\Models\Event::withTrashed()->orderBy('slug')->pluck('slug')->toArray();
+        $num = 1;
+        $slugNumbers = [];
+        
+        foreach ($slugs as $slug) {
+            if (preg_match('/-(\d+)$/', $slug, $matches)) {
+                $slugNumbers[] = intval($matches[1]);
+            }
+        }
+
+        while (in_array($num, $slugNumbers)) {
+            $num++;
+        }
+
+        $slug = $slugBase . '-' . $num;
+
+        if(preg_match('/-(\d+)$/', $slugBase, $matches)){
+            if(!in_array($matches[1],$slugNumbers)){
+                $slug=$slugBase;   
+            }
+        }
+        return $slug;
+    }
 }
+
