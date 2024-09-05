7687b1fdd129896dba44be7409c662f2b018dfce
MatteoRuffa <cortese_17@protonmail.com>
2024-09-05 12:22:03 +0200

    sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'Boolpress')</title>
+    <title>@yield('title', 'Bmol')</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
